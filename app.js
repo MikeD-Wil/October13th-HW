99fcf89dd1be1e3410d5ba46cf0bc52e27098a94
Mike <mikedon.wil@gmail.com>
2021-10-14 08:25:11 -0400

    new messasge

---
+++ app.js
@@ -0,0 +1,19 @@
+var a = 7;
+var b = 20;
+var c = 5;
+var d = 3;
+var e = 9;
+var f = 6;
+var g = 11;
+var h = 15;
+var i = 4;
+var j = 17;
+
+
+console.log(a+b);
+console.log(a-c);
+console.log(b/c);
+console.log(b*c);
+console.log(j+d+e);
+console.log(h-g);
+console.log(f*b);
